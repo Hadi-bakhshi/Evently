b88b3106c9786569087c9702b05d62c35e4d5f63
Hadi Bakhshi <hadibakhshi277@gmail.com>
2024-12-06 19:19:23 +0330

    build: update to .NET 9 and install scalar instead of swagger

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -1,16 +1,22 @@
+using Scalar.AspNetCore;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 WebApplication app = builder.Build();
 
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapScalarApiReference(); // scalar/v1
+    app.MapOpenApi();
 }
 
+app.MapGet("/", () => "Hello world!");
+
+#pragma warning disable S6966
 app.Run();
+
---
+++ src/Modules/Events/Evently.Modules.Events.Api/Database/Schema.cs
@@ -0,0 +1,6 @@
+﻿namespace Evently.Modules.Events.Api.Database;
+
+internal static class Schema
+{
+    internal const string Events = "events";
+}
