58039f58362599a826e104ba3864c27f45848316
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-01-16 19:46:43 +0330

    Add project files.

---
+++ src/API/Evently.Api/Program.cs
@@ -0,0 +1,34 @@
+using Evently.Api.Extensions;
+using Evently.Common.Application;
+using Evently.Common.Infrastructure;
+using Evently.Modules.Events.Infrastructure;
+using Scalar.AspNetCore;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+
+builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
+builder.Services.AddEventsModule(builder.Configuration);
+
+WebApplication app = builder.Build();
+
+app.MapOpenApi();
+app.MapScalarApiReference(options =>
+{
+    options.WithTitle("Evently");
+}); // scalar/v1
+
+if (app.Environment.IsDevelopment())
+{
+    app.ApplyMigrations();
+}
+
+EventsModule.MapEndpoints(app);
+
+#pragma warning disable S6966
+app.Run();
+
---
+++ src/Modules/Events/Evently.Modules.Events.Domain/Events/EventPublishedDomainEvent.cs
@@ -0,0 +1,9 @@
+﻿using Evently.Common.Domain;
+
+namespace Evently.Modules.Events.Domain.Events;
+
+public sealed class EventPublishedDomainEvent(Guid eventId) : DomainEvent
+{
+    public Guid EventId { get; init; } = eventId;
+}
+
