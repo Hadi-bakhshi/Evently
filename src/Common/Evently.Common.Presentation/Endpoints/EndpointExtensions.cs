ad9b26ba8b018bc52be9e8b3f71ca88b6ca09780
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-02-14 13:08:00 +0330

    doc: add some comments to endpoint extensions

--- src/Common/Evently.Common.Presentation/Endpoints/EndpointExtensions.cs
+++ src/Common/Evently.Common.Presentation/Endpoints/EndpointExtensions.cs
@@ -8,8 +8,17 @@ namespace Evently.Common.Presentation.Endpoints;
 
 public static class EndpointExtensions
 {
+    /// <summary>
+    /// This method scans the provided assemblies for classes that implement the IEndpoint 
+    /// interface and registers them as transient services in the DI container.
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="assemblies"></param>
+    /// <returns></returns>
     public static IServiceCollection AddEndpoints(this IServiceCollection services, params Assembly[] assemblies)
     {
+        // we'll search for the classes that implemented I Endpoint and
+        // register them as transient
         ServiceDescriptor[] serviceDescriptors = assemblies
             .SelectMany(a => a.GetTypes())
             .Where(type => type is { IsAbstract: false, IsInterface: false } &&
@@ -17,15 +26,24 @@ public static class EndpointExtensions
             .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type))
             .ToArray();
 
+        // ensures that duplicate registrations are avoided
         services.TryAddEnumerable(serviceDescriptors);
 
         return services;
     }
 
+    /// <summary>
+    /// This method retrieves all registered IEndpoint implementations from the DI container 
+    /// and maps them to the application's routing system.
+    /// </summary>
+    /// <param name="app"></param>
+    /// <param name="routeGroupBuilder"></param>
+    /// <returns></returns>
     public static IApplicationBuilder MapEndpoints(
         this WebApplication app,
         RouteGroupBuilder? routeGroupBuilder = null)
     {
+        // Retrieves all registered implementations of IEndpoint from the DI container
         IEnumerable<IEndpoint> endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
 
         IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
