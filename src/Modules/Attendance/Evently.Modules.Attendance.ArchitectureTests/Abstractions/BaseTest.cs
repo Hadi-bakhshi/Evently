500d82df38978791a5dc5e381bb1bcf1911f55dc
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-03-30 20:42:35 +0330

    feat: add architecture testing
    create tests for all modules

---
+++ src/Modules/Attendance/Evently.Modules.Attendance.ArchitectureTests/Abstractions/BaseTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using Evently.Modules.Attendance.Domain.Attendees;
+using Evently.Modules.Attendance.Infrastructure;
+
+
+namespace Evently.Modules.Attendance.ArchitectureTests.Abstractions;
+
+#pragma warning disable CA1515 // Consider making public types internal
+public abstract class BaseTest
+#pragma warning restore CA1515 // Consider making public types internal
+{
+    protected static readonly Assembly ApplicationAssembly = typeof(Application.AssemblyReference).Assembly;
+    protected static readonly Assembly DomainAssembly = typeof(Attendee).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(AttendanceModule).Assembly;
+    protected static readonly Assembly PresentationAssembly = typeof(Attendance.Presentation.AssemblyReference).Assembly;
+}
