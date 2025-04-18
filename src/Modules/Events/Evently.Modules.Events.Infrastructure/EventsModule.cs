6828f76e5d2472ade1bff9167b40d069c5603515
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-04-18 13:54:23 +0330

    feat: add saga pattern and orchestration

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/EventsModule.cs
@@ -12,6 +12,9 @@ using Evently.Modules.Events.Infrastructure.Events;
 using Evently.Modules.Events.Infrastructure.Inbox;
 using Evently.Modules.Events.Infrastructure.Outbox;
 using Evently.Modules.Events.Infrastructure.TicketTypes;
+using Evently.Modules.Events.Presentation.Events.CancelEventSaga;
+using MassTransit;
+using MassTransit.Configuration;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
@@ -37,6 +40,13 @@ public static class EventsModule
         return services;
     }
 
+    public static Action<IRegistrationConfigurator> ConfigureConsumers(string redisConnectionString)
+    {
+        return registrationConfigurator => registrationConfigurator
+            .AddSagaStateMachine<CancelEventSaga, CancelEventState>()
+            .RedisRepository(redisConnectionString);
+    }
+     
     private static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<EventsDbContext>((sp, options) =>
