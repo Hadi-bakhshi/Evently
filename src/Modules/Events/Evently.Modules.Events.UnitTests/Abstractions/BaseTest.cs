570dad3c9fa688d6a668621a2ea1ac47ce52bc7f
Hadi Bakhshi <hadibakhshi277@gmail.com>
2025-04-18 22:40:39 +0330

    feat: add some unit tests

---
+++ src/Modules/Events/Evently.Modules.Events.UnitTests/Abstractions/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using Bogus;
+using Evently.Common.Domain;
+
+namespace Evently.Modules.Events.UnitTests.Abstractions;
+
+#pragma warning disable CA1515
+public abstract class BaseTest
+{
+    protected static readonly Faker Faker = new();
+
+    public static T AssertDomainEventWasPublished<T>(Entity entity)
+    {
+        T? domainEvent = entity.DomainEvents.OfType<T>().SingleOrDefault();
+
+        if (domainEvent is null)
+        {
+            throw new Exception($"Domain event of type {typeof(T).Name} was not published.");
+        }
+
+        return domainEvent;
+    }
+}
